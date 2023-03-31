467e4056c3bd800173919dad985b9d0907d0fa0a
Abdul Baseer <105067957+AbdulBaseer7896@users.noreply.github.com>
2023-03-31 17:27:32 +0000

    In this change I add so basic of html like footer and header and also main body

--- app.py
+++ app.py
@@ -4,38 +4,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Pakistan Islamabad',
-    'salary': 'Rs 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Pakistan Lahore',
-    'salary': 'Rs 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Enginerr',
-    'location': 'usa',
-    # 'salary': 'Rs 12,00,000'
-  },
-  {
-    'id': 4,
-    'title': 'Back-end Enginerr',
-    'location': 'Remore',
-    'salary': 'Rs &120,00'
-  }
-]
-
 
 @app.route("/")
 def hello_world():
   print("this is web page")
-  return render_template("home.html", name="Qazi Abdul", jobs=JOBS)
+  return render_template("home.html")
 
 
 if __name__ == '__main__':
